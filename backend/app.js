3698484df0f87d85195629c255449bb4c0abbf18
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2022-11-07 03:27:26 -0500

    fixes

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ import logger from "morgan";
 import path from "node:path";
 import {fileURLToPath} from "node:url";
 
-import blogRouter from "./routes/blog.js";
 import indexRouter from "./routes/index.js";
 
 const app = express();
@@ -16,18 +15,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 
 app.use("/", indexRouter);
-app.use(
-  "/blog",
-  (req, res, next) => {
-    //
-    // check for password soon
-    // maybe on wed
-  },
-  blogRouter
-);
+app.use(express.static(path.join(__dirname, "public")));
 
 export default app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", {title: "Express"});
+  return res.send({hello: "world"});
 });
 
 export default router;
